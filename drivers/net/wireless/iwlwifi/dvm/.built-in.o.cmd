10b1f88d5ba2965c4fefc002e56da23777833563
Alejandro Lozano <alejandro.lozano@freescale.com>
2017-06-05 16:59:15 -0500

    Adding Linux Source Code

---
+++ drivers/net/wireless/iwlwifi/dvm/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/net/wireless/iwlwifi/dvm/built-in.o :=  arm-poky-linux-gnueabi-ld -EL    -r -o drivers/net/wireless/iwlwifi/dvm/built-in.o drivers/net/wireless/iwlwifi/dvm/iwldvm.o 
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#7 SMP PREEMPT Fri Apr 28 12:08:19 CDT 2017"
+#define LINUX_COMPILE_BY "tic-imx"
+#define LINUX_COMPILE_HOST "tic-imx"
+#define LINUX_COMPILER "gcc version 5.2.0 (GCC) "
